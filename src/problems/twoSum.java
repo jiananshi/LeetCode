5de8670044955707b7237884b07be2d6055e463c
jiananshi <klamtine@gmail.com>
2019-03-11 16:27:10 +0800

    $feat: better performance with O(n) size

--- src/problems/twoSum.java
+++ src/problems/twoSum.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int sp = 0;
-        
-        while (sp < nums.length - 1) {
-            int base = nums[sp];
-            int expected = target - base;
-            int fp = sp + 1;
-            while (fp < nums.length) {
-                if (nums[fp] == expected) {
-                    int[] result = {sp, fp};
-                    return result;
-                }
-                fp++;
-            }
-            sp++;
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int[] result = {0, 0};
+
+        for (int i = 0; i < nums.length; i++) {
+          int num = nums[i];
+          if (m.containsKey(num)) {
+            result[0] = m.get(num);
+            result[1] = i;
+            break;
+          }
+          m.put(target - num, i);
         }
-        return null;
+        
+        return result;
     }
 }
 
