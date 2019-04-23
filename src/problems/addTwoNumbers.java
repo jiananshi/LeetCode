49100a4f72d7f73cbfb24652a8b9ea07779f5d68
jiananshi <klamtine@gmail.com>
2019-04-23 22:39:05 +0800

    $feat: addTwoNumbers

---
+++ src/problems/addTwoNumbers.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int base = 0;
+        
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + base;
+            base = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        
+        if (base != 0) {
+            current.next = new ListNode(base);
+        }
+        
+        return head.next;
+    }
+}
+
