921b16de8404584a167832db71daec6918cdce60
jiananshi <klamtine@gmail.com>
2019-04-24 13:55:54 +0800

    $feat: update 2SumII with out sorting

--- src/problems/twoSumII.java
+++ src/problems/twoSumII.java
@@ -1,34 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] result = {0, 0};
-        for (int i = 0; i < numbers.length; i++) {
-            int expected = target - numbers[i];
-            int found = binarySearch(numbers, i + 1, expected);
-            if (found != -1) {
-                result[0] = i + 1;
-                result[1] = found + 1;
-                break;
+        int fp = 0;
+        int tp = numbers.length - 1;
+        
+        while (fp < tp) {
+            int sum = numbers[fp] + numbers[tp];
+            if (sum == target) {
+                return new int[]{fp + 1, tp + 1};
+            } else if (sum > target) {
+                tp--;
+            } else {
+                fp++;
             }
         }
-        return result;
-    }
-    
-    public int binarySearch(int[] nums, int start, int target) {
-      if (nums.length == 0) return -1;
-      int mid;
-      int end = nums.length - 1;
-      while (start <= end) {
-        mid = (end - start) / 2 + start;
-        int val = nums[mid];
-        if (target < val) {
-          end = mid - 1;
-        } else if (target > val) {
-          start = mid + 1;
-        } else {
-          return mid;
-        }
-      }
-      return -1;
+        
+        return null;
     }
 }
 
