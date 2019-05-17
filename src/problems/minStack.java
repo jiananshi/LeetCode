a30b64f234b1041c0d31fd99082786700a2551af
jiananshi <klamtine@gmail.com>
2019-05-17 10:38:16 +0800

    $feat: add min-stack

---
+++ src/problems/minStack.java
@@ -0,0 +1,27 @@
+class MinStack {
+  private int min = Integer.MAX_VALUE;
+  private Stack<Integer> stack = new Stack<>();
+
+  public MinStack() {}
+
+  public void push(int x) {
+    if (x < min) {
+      stack.push(min);
+      min = x;
+    }
+    stackA.push(x);
+  }
+
+  public void pop() {
+    if (stack.pop() == min) min = stack.pop();
+  }  
+
+  public int top() {
+    return stack.peek();
+  }
+
+  public int getMin() {
+    return min;
+  }
+}
+
